a11cb510affa7a8288facc5113353930280beac0
zunzum <zunzum@naver.com>
2022-10-08 20:07:36 +0900

    돌리기 성공 마지막 왜 멈추는지 모르겠음

--- app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
@@ -2,10 +2,14 @@ package com.zunzum.fakesecurity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        dolgi.startAnimation(animation)
     }
 }
