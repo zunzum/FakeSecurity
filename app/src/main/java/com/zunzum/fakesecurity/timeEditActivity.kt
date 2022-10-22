90f51d7a2a86b04d5add8470b287dedce5a0fd9d
zunzum <zunzum@naver.com>
2022-10-22 09:27:20 +0900

    시간 값을 받아 처리할 Activity추가

---
+++ app/src/main/java/com/zunzum/fakesecurity/timeEditActivity.kt
@@ -0,0 +1,11 @@
+package com.zunzum.fakesecurity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class timeEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_edit)
+    }
+}
