32165f739b35e54ab1664a1701aa673d2720ede8
zunzum <zunzum@naver.com>
2022-10-22 22:03:55 +0900

    아무것도 안해도 켤때마가 당일 아침 0730부터 시작하게 변경 전체 수정

--- app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
@@ -31,77 +31,79 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var insDay = 1666429408278 //차단일시 눌러서 변환할 변수 선언
-        val dateFormat = SimpleDateFormat("d일\nhh:mm:ss") //가운데 돌아가는거 시간형식 지정해주기
-        val upDateFormat = SimpleDateFormat("yy.MM.dd hh:mm") //차단일시형식 지정해주기
+
+        var insDay = SimpleDateFormat("yyyy-MM-dd").format(Date(System.currentTimeMillis())) //어플 켠 시간 중 연월일까지만 받아오기
+        Log.d("확인 어플 켠 시간 연월일","$insDay")
+        var inchant = "$insDay 07:30:00" //연월일 + 시간 추가
+        Log.d("확인 연월일 시간추가","$inchant")
+        var inchantDay = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(inchant) //시간까지 포함하여 변환
+        Log.d("확인 시간까지 포함해서 변환","$inchantDay")
+        var inDay = inchantDay.time //밀리세컨으로 변환
+        Log.d("확인 밀리세컨으로 변환","$inDay")
+
+        val dateFormat = SimpleDateFormat("d일\nHH:mm:ss") //가운데 돌아가는거 시간형식 지정해주기
+        val upDateFormat = SimpleDateFormat("yy.MM.dd HH:mm") //차단일시형식 지정해주기
+        var chadan = upDateFormat.format(Date(inDay))
+        insDayTxt.text = "차단일시 : ${chadan}"
+
 
         val animation = AnimationUtils.loadAnimation(this, R.anim.rotate) //animation이라는 변수 생성 후 그 안에 anim에 따로 생성해둔 kt파일 불러오기
         dolgi.startAnimation(animation) //dolgi에 시작애니메이션 넣기
 
-        val cutTime = System.currentTimeMillis() //cuttime에 어플 켠 시간 날것으로 받아오기
-        Log.d("켠 시간 날것","$cutTime")
         kotlin.concurrent.timer(period = 1000){
             //1초마다 실행될 블록 백그라운드에서 작업
-            val nowTime = System.currentTimeMillis()
-            val minusTime = dateFormat.format(Date(nowTime-insDay))
+            val nowTime = Calendar.getInstance().timeInMillis
+            val chadanDate = nowTime-inDay
+            val chadanDay = chadanDate/(24*60*60*1000)
+            val chadanHour = chadanDate/(60*60*1000)
+            val chadanMin = (chadanDate%(60*60*1000))/(60*1000)
+            val chadanSec = (chadanDate%(60*1000))/1000
+//            val minusTime = dateFormat.format(Date(nowTime-inDay))
+//            Log.d("현재시간", "${upDateFormat.format(Date(nowTime))} / $nowTime")
+//            Log.d("기준시간", "${upDateFormat.format(Date(inDay))} / $inDay")
+//            Log.d("시간차이", "${upDateFormat.format(Date(nowTime-inDay))} / ${nowTime-inDay}")
             runOnUiThread {
                 //ui상에서 표시될 블록
-                chadanTime.text = "$minusTime" //차단타임을 현재시간에서 어플켠시간을 뺀 시간으로 설정
+                chadanTime.text = "${chadanDay}일\n${chadanHour}:${chadanMin}:${chadanSec}" //차단타임을 현재시간에서 어플켠시간을 뺀 시간으로 설정
             }
         }
 
         //메뉴 버튼을 누르면 할 동작
         menuBtn.setOnClickListener {
-//            //상수로 myIntent라는 변수명을 설정,여기서출발, 목적지 입력
-//            val myIntent = Intent(this,timeEditActivity::class.java)
-//            //가서 뭘 받아오는 명령어(위에 설정해놨던 myIntent실행, 뭘 받아올건지 태그)ㄴ
-//            startActivityForResult(myIntent, REQUEST_FOR_STARTDATE)
+            val timeEditIntent = Intent(this,timeEditActivity::class.java)
+            startActivity(timeEditIntent)
             }
         //비콘버튼 누르면 동작
         biconBtn.setOnClickListener {
-            val curTime = upDateFormat.format(Date(cutTime)) //날것의 시간을 date형식으로 받아 형식 지정
-            val alert = AlertDialog.Builder(this)
-            alert.setTitle("설정하시겠습니까?")
-                .setMessage("확인을 누르면 차단일시를 현재시간으로 설정")
-                .setPositiveButton("확인",
-                DialogInterface.OnClickListener { dialog, id ->
-                    insDayTxt.text = "차단일시 : $curTime"
-                })
-                .setNegativeButton("취소",null)
-            alert.show()
+//            val curTime = upDateFormat.format(Date(cutTime)) //날것의 시간을 date형식으로 받아 형식 지정
+//            val alert = AlertDialog.Builder(this)
+//            alert.setTitle("설정하시겠습니까?")
+//                .setMessage("확인을 누르면 차단일시를 현재시간으로 설정")
+//                .setPositiveButton("확인",
+//                DialogInterface.OnClickListener { dialog, id ->
+//                    insDayTxt.text = "차단일시 : $curTime"
+//                })
+//                .setNegativeButton("취소",null)
+//            alert.show()
         }
         //차단일시 누르면 동작
         insDayTxt.setOnClickListener {
-            val chadanDay = EditText(this)
-            chadanDay.gravity=Gravity.CENTER
-            val builder = AlertDialog.Builder(this)
-                .setTitle("원하는 일시 입력 0000-00-00")
-                .setView(chadanDay)
-                .setPositiveButton("확인",
-                DialogInterface.OnClickListener { dialogIn, id ->
-                    val inputChadanDay = chadanDay.text.toString()+" 07:30:00"  //alert에서 입력 받기
-                    var inChaDanDate = SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(inputChadanDay)    //입력받은 inputChadanDay를 지정한 포맷으로 변환
-                    var insDay = inChaDanDate.time //전역변수에 할당해주기!! 이거 되는지 모르겠네
-                    var ilsi = upDateFormat.format(Date(insDay))
-
-                    insDayTxt.text = "차단일시 : ${ilsi}"
-
-                })
-            builder.show()
+//            val chadanDay = EditText(this)
+//            chadanDay.gravity=Gravity.CENTER
+//            val builder = AlertDialog.Builder(this)
+//                .setTitle("원하는 일시 입력 0000-00-00")
+//                .setView(chadanDay)
+//                .setPositiveButton("확인",
+//                DialogInterface.OnClickListener { dialogIn, id ->
+//                    val inputChadanDay = chadanDay.text.toString()+" 07:30:00"  //alert에서 입력 받기
+//                    var inChaDanDate = SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(inputChadanDay)    //입력받은 inputChadanDay를 지정한 포맷으로 변환
+//                    var insDay = inChaDanDate.time //전역변수에 할당해주기!! 이거 되는지 모르겠네
+//                    var ilsi = upDateFormat.format(Date(insDay))
+//
+//                    insDayTxt.text = "차단일시 : ${ilsi}"
+//
+//                })
+//            builder.show()
         }
         }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        //돌아온 이유가 날자를 받으러 온게 맞는지?
-//        if (requestCode == REQUEST_FOR_STARTDATE) {
-//            //추가질문 : 확인을 눌러서 돌아온게 맞는지?
-//            if (resultCode == RESULT_OK) {
-//                //실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
-//                val newDate = data?.getStringExtra("chadan")
-//                Log.d("입력받아온 시간","$newDate")
-//                insDayTxt.text = newDate.toString()
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/zunzum/fakesecurity/timeEditActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity/timeEditActivity.kt
@@ -13,22 +13,11 @@ class timeEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_edit)
 
-//        okBtn.setOnClickListener {
-//            //입력한 날자를 변수로 저장
-////            val inputChadanDate = chadanSetTime.text.toString()+" 07:30:00"
-////            var cf = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
-////            var inChaDanDate = cf.parse(inputChadanDate)
-//
-//            //입력한 닉네임을 가지고 메인으로 복귀
-//
-//            //입력한 닉네임을 담아주기 윈한 용도로만 사용하는 Intent
-//            val resultIntent = Intent()
-//            resultIntent.putExtra("chadan", inChaDanDate)
-//            setResult(Activity.RESULT_OK, resultIntent)
-//            finish()
-//        }
-//        noBtn.setOnClickListener {
-//            finish()
-//        }
+        okBtn.setOnClickListener {
+            finish()
+        }
+        noBtn.setOnClickListener {
+            finish()
+        }
     }
 }
