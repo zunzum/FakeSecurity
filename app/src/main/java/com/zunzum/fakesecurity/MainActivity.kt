1c17ad7d90f492b7fba6c692907786b4250c673b
zunzum <zunzum@naver.com>
2022-10-22 18:25:06 +0900

    - 기능해제(비콘) 누르면 alert뜨면서 확인 누르면 켠 시간으로 차단일시 설정 - 차단일시 누르면 형식에 맞춰 입력하면 차단일시 변경
    과제 : 차단일시 눌러서 변경하는걸 가운데 일수에 적용하고 싶은데 전역변수에 어떻게 집어넣는지 모르겠음

--- app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity/MainActivity.kt
@@ -9,10 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.Gravity
 import android.view.animation.AnimationUtils
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.os.postDelayed
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.util.Calendar
 import java.util.Date
 import java.util.Timer
@@ -24,13 +28,12 @@ import kotlin.time.DurationUnit
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_FOR_STARTDATE = 1004 //날자를 가지러 가는 인텐트 멤버변수 설정
-    var insDay = 0
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var insDay = 1666429408278 //차단일시 눌러서 변환할 변수 선언
+        val dateFormat = SimpleDateFormat("d일\nhh:mm:ss") //가운데 돌아가는거 시간형식 지정해주기
+        val upDateFormat = SimpleDateFormat("yy.MM.dd hh:mm") //차단일시형식 지정해주기
 
         val animation = AnimationUtils.loadAnimation(this, R.anim.rotate) //animation이라는 변수 생성 후 그 안에 anim에 따로 생성해둔 kt파일 불러오기
         dolgi.startAnimation(animation) //dolgi에 시작애니메이션 넣기
@@ -40,8 +43,7 @@ class MainActivity : AppCompatActivity() {
         kotlin.concurrent.timer(period = 1000){
             //1초마다 실행될 블록 백그라운드에서 작업
             val nowTime = System.currentTimeMillis()
-            val dateFormat = SimpleDateFormat("d일\nhh:mm:ss") //형식 지정해주기
-            val minusTime = dateFormat.format(Date(nowTime-cutTime))
+            val minusTime = dateFormat.format(Date(nowTime-insDay))
             runOnUiThread {
                 //ui상에서 표시될 블록
                 chadanTime.text = "$minusTime" //차단타임을 현재시간에서 어플켠시간을 뺀 시간으로 설정
@@ -50,16 +52,14 @@ class MainActivity : AppCompatActivity() {
 
         //메뉴 버튼을 누르면 할 동작
         menuBtn.setOnClickListener {
-            //상수로 myIntent라는 변수명을 설정,여기서출발, 목적지 입력
-            val myIntent = Intent(this,timeEditActivity::class.java)
-            //가서 뭘 받아오는 명령어(위에 설정해놨던 myIntent실행, 뭘 받아올건지 태그)ㄴ
-            startActivityForResult(myIntent, REQUEST_FOR_STARTDATE)
+//            //상수로 myIntent라는 변수명을 설정,여기서출발, 목적지 입력
+//            val myIntent = Intent(this,timeEditActivity::class.java)
+//            //가서 뭘 받아오는 명령어(위에 설정해놨던 myIntent실행, 뭘 받아올건지 태그)ㄴ
+//            startActivityForResult(myIntent, REQUEST_FOR_STARTDATE)
             }
         //비콘버튼 누르면 동작
         biconBtn.setOnClickListener {
-            val dateFormat = SimpleDateFormat("yy.MM.dd hh:mm") //형식 지정해주기
-            val curTime = dateFormat.format(Date(cutTime)) //날것의 시간을 date형식으로 받아 형식 지정
-
+            val curTime = upDateFormat.format(Date(cutTime)) //날것의 시간을 date형식으로 받아 형식 지정
             val alert = AlertDialog.Builder(this)
             alert.setTitle("설정하시겠습니까?")
                 .setMessage("확인을 누르면 차단일시를 현재시간으로 설정")
@@ -70,18 +70,38 @@ class MainActivity : AppCompatActivity() {
                 .setNegativeButton("취소",null)
             alert.show()
         }
+        //차단일시 누르면 동작
+        insDayTxt.setOnClickListener {
+            val chadanDay = EditText(this)
+            chadanDay.gravity=Gravity.CENTER
+            val builder = AlertDialog.Builder(this)
+                .setTitle("원하는 일시 입력 0000-00-00")
+                .setView(chadanDay)
+                .setPositiveButton("확인",
+                DialogInterface.OnClickListener { dialogIn, id ->
+                    val inputChadanDay = chadanDay.text.toString()+" 07:30:00"  //alert에서 입력 받기
+                    var inChaDanDate = SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(inputChadanDay)    //입력받은 inputChadanDay를 지정한 포맷으로 변환
+                    var insDay = inChaDanDate.time //전역변수에 할당해주기!! 이거 되는지 모르겠네
+                    var ilsi = upDateFormat.format(Date(insDay))
+
+                    insDayTxt.text = "차단일시 : ${ilsi}"
+
+                })
+            builder.show()
+        }
         }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        //돌아온 이유가 날자를 받으러 온게 맞는지?
-        if (requestCode == REQUEST_FOR_STARTDATE) {
-            //추가질문 : 확인을 눌러서 돌아온게 맞는지?
-            if (resultCode == RESULT_OK) {
-                //실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
-                val newDate = data?.getStringExtra("chadan")
-                insDay = 2022  //insDay는 맨 앞에 설정해둔 변수. 현재시간과 비교할 예정
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        //돌아온 이유가 날자를 받으러 온게 맞는지?
+//        if (requestCode == REQUEST_FOR_STARTDATE) {
+//            //추가질문 : 확인을 눌러서 돌아온게 맞는지?
+//            if (resultCode == RESULT_OK) {
+//                //실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
+//                val newDate = data?.getStringExtra("chadan")
+//                Log.d("입력받아온 시간","$newDate")
+//                insDayTxt.text = newDate.toString()
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/zunzum/fakesecurity/timeEditActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity/timeEditActivity.kt
@@ -4,27 +4,31 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_time_edit.*
+import java.text.SimpleDateFormat
 
 class timeEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_edit)
 
-        okBtn.setOnClickListener {
-            //입력한 날자를 변수로 저장
-            val inputChadanDate = chadanSetTime.text.toString()
-
-            //입력한 닉네임을 가지고 메인으로 복귀
-
-            //입력한 닉네임을 담아주기 윈한 용도로만 사용하는 Intent
-            val resultIntent = Intent()
-            resultIntent.putExtra("chadan", inputChadanDate)
-            setResult(Activity.RESULT_OK, resultIntent)
-            finish()
-        }
-        noBtn.setOnClickListener {
-            finish()
-        }
+//        okBtn.setOnClickListener {
+//            //입력한 날자를 변수로 저장
+////            val inputChadanDate = chadanSetTime.text.toString()+" 07:30:00"
+////            var cf = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+////            var inChaDanDate = cf.parse(inputChadanDate)
+//
+//            //입력한 닉네임을 가지고 메인으로 복귀
+//
+//            //입력한 닉네임을 담아주기 윈한 용도로만 사용하는 Intent
+//            val resultIntent = Intent()
+//            resultIntent.putExtra("chadan", inChaDanDate)
+//            setResult(Activity.RESULT_OK, resultIntent)
+//            finish()
+//        }
+//        noBtn.setOnClickListener {
+//            finish()
+//        }
     }
 }
